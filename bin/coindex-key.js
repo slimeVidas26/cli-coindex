9992ebc4b6769a590319aff391f69faa4f70a1a7
SlimeVidas <dahan.isaac@gmail.com>
2021-03-22 22:19:32 +0200

    create-coindex-key.js

---
+++ bin/coindex-key.js
@@ -0,0 +1,8 @@
+const program = require('commander');
+
+
+program
+    .command('set' , 'set API key ----- get at https://nomics.com')
+
+
+program.parse(process.argv)
--- bin/coindex.js
+++ bin/coindex.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
+const program = require('commander');
+const pkg = require('../package.json');
+
+program
+       .version(pkg.version)
+       .command('key' , 'Manage API key https://nomics.com')
+
+
 console.log("Hello from coindex")
+
+
+
+
+program.parse(process.argv)
